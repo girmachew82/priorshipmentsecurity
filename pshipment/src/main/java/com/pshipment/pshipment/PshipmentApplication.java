158a07318e43a7a8aa6cdd81e3a31f0e72877b0a
girmachew82 <girmachewabebe@gmail.com>
2021-06-30 17:57:25 +0300

    first commit

---
+++ pshipment/src/main/java/com/pshipment/pshipment/PshipmentApplication.java
@@ -0,0 +1,13 @@
+package com.pshipment.pshipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PshipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PshipmentApplication.class, args);
+	}
+
+}
---
+++ pshipment/src/test/java/com/pshipment/pshipment/PshipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pshipment.pshipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PshipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
